3fe17d899c20b61bd91100daaf3ac3ea92193a4c
hugovk <hugovk+github@gmail.com>
2013-10-25 11:58:08 +0300

    Remove duplicates from input and create bigger word clouds
    Doesn't remove duplicate words, but duplicate logged tweets.

--- new_favourite_words.sh
+++ new_favourite_words.sh
@@ -1,14 +1,20 @@
 CSV_FILE=/TODO/full/path/to/newfavouritewords.csv
 
-grep ",is my new favourite word," $CSV_FILE > /tmp/new_favourite_words.tmp
-grep ",is my new favorite word,"  $CSV_FILE > /tmp/new_favorite_words.tmp
-grep ",is my new fave word,"      $CSV_FILE > /tmp/new_fave_words.tmp
+DEDUPE=/tmp/newfavouritewords.csv
+rm $DEDUPE
+awk '!a[$0]++' $CSV_FILE > $DEDUPE
+
+rm /tmp/new_fa*.tmp
+grep ",is my new favourite word," $DEDUPE > /tmp/new_favourite_words.tmp
+grep ",is my new favorite word,"  $DEDUPE > /tmp/new_favorite_words.tmp
+grep ",is my new fave word,"      $DEDUPE > /tmp/new_fave_words.tmp
 
 # Slice out first column containing just the fav[o[u]ri]te word
+rm /tmp/new_fa*.lst
 cat /tmp/new_favourite_words.tmp | cut -d, -f1 > /tmp/new_favourite_words.lst
 cat /tmp/new_favorite_words.tmp  | cut -d, -f1 > /tmp/new_favorite_words.lst
 cat /tmp/new_fave_words.tmp      | cut -d, -f1 > /tmp/new_fave_words.lst
-cat $CSV_FILE                    | cut -d, -f1 > /tmp/new_favourite_favorite_and_fave_words.lst
+cat $DEDUPE                      | cut -d, -f1 > /tmp/new_favourite_favorite_and_fave_words.lst
 
 # Find top 10s
 for f in favourite favorite fave favourite_favorite_and_fave
@@ -19,8 +25,12 @@ done
 
 # Make word clouds (uses https://github.com/hugovk/word_cloud)
 
+rm /tmp/new_fa*.png
 for f in favourite favorite fave favourite_favorite_and_fave
 do
     echo "Create word cloud for 'is my new $f word'"
-    python /TODO/full/path/to/word_cloud/wordcloud.py --stopwords None --width 800 --height 400 --fontfile "/Library/Fonts/Times New Roman.ttf" "/tmp/new_${f}_words.lst" -o "/tmp/new_${f}_words.png"
+    python /TODO/full/path/to/word_cloud/wordcloud.py --stopwords None --width 1024 --height 576 --fontfile "/Library/Fonts/Times New Roman.ttf" "/tmp/new_${f}_words.lst" -o "/tmp/new_${f}_words.png"
+
 done
+
+echo "Done."
