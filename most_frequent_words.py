a265994a4d2d81938e87aa0c173c7d33420825af
hugovk <hugovk@users.noreply.github.com>
2013-12-20 17:27:24 +0200

    Move main parts to functions so it can be imported.

--- most_frequent_words.py
+++ most_frequent_words.py
@@ -2,13 +2,23 @@
 """
 Show the most common words of a newline-separated word list
 """
-import argparse
+from collections import Counter
 
 # Add thousands commas
 def commafy(value):
     return "{:,}".format(value)
 
+def most_frequent_words_and_counts(word_list, number=None):
+    counter = Counter(word_list)
+    most_common = counter.most_common(number)
+    return most_common
+
+def most_frequent_words(word_list, number=None):
+    words = [ite for ite, it in most_frequent_words_and_counts(word_list, number)]
+    return words
+
 if __name__ == "__main__":
+    import argparse
     parser = argparse.ArgumentParser(description="Show the most common words of a newline-separated word list",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('infile',
@@ -18,11 +28,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     word_list = open(args.infile).read().splitlines()
-    from collections import Counter
-    counter = Counter(word_list)
-    most_common = counter.most_common(args.number)
+    most_common = most_frequent_words_and_counts(word_list, args.number)
 
-    print args.infile, "contains", commafy(len(word_list)), "words and", commafy(len(counter.most_common())), "unique words.\nThe top", commafy(args.number), "words are:"
+    print args.infile, "contains", commafy(len(word_list)), "words and", commafy(len(most_frequent_words_and_counts(word_list))), "unique words.\nThe top", commafy(args.number), "words are:"
     for i, (word, count) in enumerate(most_common):
         print commafy(i+1) + ".", word, "(" + commafy(count) + ")"
 
