47274f059e87648c8ab4c0c9bbbfdf0d00b5d21d
Hugo <hugovk@users.noreply.github.com>
2018-11-13 18:12:28 +0200

    Helper script to collect words

--- collect.sh
+++ collect.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+cd ~/github/word-tools/
+./update.sh
+
 for BOT in nixibot favibot lovihatibot
 do
     python2 $BOT.py --tweet none --ini ~/bin/data/$BOT.ini --csv ~/bin/data/$BOT.csv
--- crontask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd ~/github/word-tools/
-./update.sh
-./run.sh
